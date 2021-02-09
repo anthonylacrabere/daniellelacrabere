859089fa2aa07a91c0258b4a406c3e753f354802
lacra <anthony.lacrabere@gmail.com>
2021-02-09 11:19:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [people, setPeople] = useState();
+
+  const fetchData = async () => {
+    const response = await fetch("https://swapi.dev/api/people");
+
+    const json = await response.json();
+
+    if (json) {
+      setPeople(json.results);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log(people);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,5 @@
+const test = (props) => {
+  return <h1>SALUT NONO, TU AS {props.age}</h1>;
+};
+
+export default test;
